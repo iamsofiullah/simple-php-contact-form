671b5cd147b92c5ace9c7eec24fe8d14b8377200
Sofiullah Chowdhury <67284020+iamsofiullah@users.noreply.github.com>
2022-04-20 11:33:51 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "sofiullahc@gmail.com";
+$recipient = "example@gmail.com";
 
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
